cc2d97df4963a40c9b0864bbdedc29e8234e9e5b
lordie <levimanga@gmail.com>
2021-07-08 20:54:54 -0300

    fixes table overflow on mobile

--- src/Components/PageLayout.tsx
+++ src/Components/PageLayout.tsx
@@ -1,16 +1,40 @@
-import { Layout } from "antd";
-import React from "react";
+import { UserOutlined } from "@ant-design/icons";
+import { Avatar, Layout, Space, Typography } from "antd";
+import React, { ReactNode } from "react";
 
 import { colors } from "../../theme";
 
-export default function PageLayout({ children }: { children: JSX.Element }) {
+export default function PageLayout({ children }: { children: ReactNode }) {
   return (
-    <Layout>
-      <Layout.Header>Company</Layout.Header>
-      <Layout.Content
-        style={{ backgroundColor: colors.backgroundColor, padding: "5vh 0" }}
+    <Layout style={{ alignItems: "center" }}>
+      <Layout.Header
+        style={{
+          position: "fixed",
+          width: "100%",
+          zIndex: 999,
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
       >
-        {children}
+        <Typography.Title level={4} style={{ margin: 0 }}>
+          Company
+        </Typography.Title>
+        <Avatar icon={<UserOutlined />} />
+      </Layout.Header>
+      <Layout.Content
+        style={{
+          marginTop: 64,
+          padding: "24px 5vw",
+          maxWidth: "1000px",
+          width: "100vw",
+          minHeight: "100vh",
+          backgroundColor: colors.backgroundColor,
+        }}
+      >
+        <Space size="large" direction="vertical" style={{ width: "100%" }}>
+          {children}
+        </Space>
       </Layout.Content>
       <Layout.Footer>Lordie and AntDesign</Layout.Footer>
     </Layout>
